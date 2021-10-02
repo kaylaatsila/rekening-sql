7dbf011edbcd0f20fa6db0378ee468b715b79b75
jjbhvgmhg <90822736+jjbhvgmhg@users.noreply.github.com>
2021-10-02 08:34:22 +0700

    Update Rekening.java

--- src/projekFinal/Rekening.java
+++ src/projekFinal/Rekening.java
@@ -1,4 +1,7 @@
 class Rekening{
     private IntegerProperty noRekening;
     private DoubleProperty saldo;
-
+public Rekening(int noRekening, double saldo) {
+        this.noRekening = new SimpleIntegerProperty(noRekening);
+        this.saldo = new SimpleDoubleProperty(saldo);
+    }
